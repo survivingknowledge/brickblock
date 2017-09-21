0b0c6a44c9dae435f947e5c110a535ce80016ea8
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-21 19:45:03 -0400

    Navbar with mobile menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import '../styles/app.css';
 import '../styles/base.css';
-import '../styles/nav.css';
+
+import Navbar from './Navbar';
+
 
 import FA from 'react-fontawesome';
 
@@ -9,22 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <nav>
-            <div className="container">
-              <div className="row">
-                <div className="branding"><a href="/">Blockbrick</a></div>
-                <div className="mobile-toggle-menu" data-toggle='menu'><FA name='bars' /></div>
-                <ul className="menu">
-                  <li className='active'><a href="/profile">Profile</a></li>
-                  <li><a href="/settings">Settings</a></li>
-                  <li><a href="/signin">Sign in</a></li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <div className="container">
-            <div className="row">hello</div>
+        <div className="container">
+          <div className="row">
+            <header>
+              <Navbar />
+            </header>
           </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/nav.css';
+import FA from 'react-fontawesome';
+
+const Navbar = (props) => {
+  return (
+    <nav>
+      <div className="branding"><a href="/">Blockbrick</a></div>
+      <div className="mobile-toggle-menu"><FA name='bars' /></div>
+      <ul className="menu">
+        <li className='active'><a href="/profile">Profile</a></li>
+        <li><a href="/settings">Settings</a></li>
+        <li><a href="/signin">Sign in</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
