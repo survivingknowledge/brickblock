43777bf1ba0bb13eff4c62338897299c76df9ff7
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-22 21:33:21 -0400

    Chartjs as a doughnut chart

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,73 @@ import Fundstats from './Fundstats';
 import Fancycharts from './Fancycharts';
 
 import Navbar from './Navbar';
+import Chart from 'chart.js';
 
 class App extends Component {
+
+  componentDidMount() {
+    //version 2 of Charjs
+    Chart.pluginService.register({
+      beforeDraw: function(chart) {
+        var width = chart.chart.width,
+            height = chart.chart.height,
+            ctx = chart.chart.ctx;
+
+        ctx.restore();
+        var fontSize = (height / 250).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        //text color
+        ctx.fillStyle = '#0091ea';
+
+        var text = "281% USD",
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      }
+    });
+
+    var ctx = document.getElementById("donut-chart").getContext('2d');
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [15, 10, 75],
+          borderColor: [
+              'rgba(97,32,246,1)',
+              'rgba(0, 247, 154, 1)',
+              'rgba(0, 146, 238, 1)',
+            ],
+            backgroundColor: [
+              'rgba(97,32,246,1)',
+              'rgba(0, 247, 154, 1)',
+              'rgba(0, 146, 238, 1)',
+            ],
+            borderWidth: 1,
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
+        ],
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+        rotation: 5,
+        responsive: true,
+        maintainAspectRatio: true,
+      }
+
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
--- src/components/Fancycharts.js
+++ src/components/Fancycharts.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Fancycharts = (props) => {
   return (
     <div className="fancy-charts">
-      <div className="col">Image</div>
+      <div className="col"><canvas id="donut-chart" width="150" height="150"></canvas></div>
       <div className="col">s</div>
       <div className="col">s</div>
     </div>
